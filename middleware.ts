468532415cb0a8c817fa655a6199cc6c172f3e32
JeremyTjahjana <jeremytjahjana@gmail.com>
2026-02-24 13:32:27 +0700

    update komen v.2

--- middleware.ts
+++ middleware.ts
@@ -5,9 +5,9 @@ const isProtectedRoute = createRouteMatcher([
   "/problems/[id]",
 ]);
 
-export default clerkMiddleware((auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) {
-    auth().protect();
+    await auth().protect();
   }
 });
 
